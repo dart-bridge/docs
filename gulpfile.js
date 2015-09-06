db9ab63b6b15135d9fb7019bb87be78edb35de59
Emil Persson <emil.n.persson@gmail.com>
2015-09-06 13:42:53 +0200

    Modification

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 
 var build = require('./build/build');
 gulp.task('build:build', build.build);
-gulp.task('build:commit', build.commit);
-gulp.task('build', ['build:build', 'build:commit']);
+gulp.task('build:commit', ['build:build'], build.commit);
+gulp.task('build', ['build:commit']);
 
 gulp.task('deploy', require('./build/deploy'));
 
--- src/index.md
+++ src/index.md
@@ -1,3 +1,3 @@
 # Bridge
 
-Some content
+Some more content
